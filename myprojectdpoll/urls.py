90c95a1d1e7a2bc1a51a64b9341ee3551724da0e
Muhsina Beegum <151713066+muhsina419@users.noreply.github.com>
2025-06-04 11:40:19 +0530

    cleaned duplicates in urls.py

--- myprojectdpoll/urls.py
+++ myprojectdpoll/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
     path('reg_success/<str:unique_id>/', views.reg_success, name='reg_success'),
     path('reg_failure/', views.reg_failure, name='reg_failure'),
     path('admin/poll-results/<int:poll_id>/', views.poll_results, name='poll_results'),
-    path('admin/poll-results/<int:poll_id>/', views.poll_results, name='poll_results'),
     path('get-unique-id/', views.get_unique_id, name='unique_id'),
     path('unique-id/', views.unique_id_view, name='unique_id'),
     
