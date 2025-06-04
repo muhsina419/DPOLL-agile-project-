a3c4db86ebfce5411178b294dc9cce379eaafc49
Muhsina Beegum <151713066+muhsina419@users.noreply.github.com>
2025-06-04 20:02:57 +0530

    Update settings.py

--- dpoll/settings.py
+++ dpoll/settings.py
@@ -61,10 +61,7 @@ WSGI_APPLICATION = 'dpoll.wsgi.application'
 
 # Database configuration (PostgreSQL)
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
